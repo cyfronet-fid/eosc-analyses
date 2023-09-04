46f44ea3c3bd433fb595455c7826ae7cf27711fc
Wiktor Florian <wiktorflorianwf@gmail.com>
2023-09-04 09:47:59 +0200

    data_loader update

--- urls_response_codes/data_loader.py
+++ urls_response_codes/data_loader.py
@@ -1,14 +1,29 @@
-import config
-import boto3
 import json
-from boto3.session import Session
-import pandas as pd
+import logging
 from typing import List, Dict, Any
 
-def connect_to_s3(access_key: str, secret_key: str, endpoint: str) -> Session:
-    """Connect to s3"""
-    session = boto3.session.Session()
+import numpy as np
+import pandas as pd
 
+import boto3
+from boto3.session import Session
+
+import config
+
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('logfile.log')
+    ]
+)
+
+
+def connect_to_s3(access_key: str, secret_key: str, endpoint: str) -> boto3.client:
+    """Connect to Amazon S3"""
+    session = Session()
     s3_client = session.client(
         service_name="s3",
         aws_access_key_id=access_key,
@@ -30,7 +45,7 @@ def process_object(obj: bytes, selected_fields: List[str]) -> pd.DataFrame:
                 row = {field: doc.get(field) for field in selected_fields}
                 rows.append(row)
             except json.JSONDecodeError as e:
-                print(f"Invalid JSON object: {line}, error: {str(e)}")
+                logging.error("Invalid JSON object: %s, errorL %s", line, str(e))
     df = pd.DataFrame(rows)
     new_df = df.apply(compute_row_values, axis=1, result_type='expand')
     return new_df
@@ -48,6 +63,10 @@ def compute_row_values(row: pd.Series) -> Dict[str, Any]:
         'number_unique_urls': unique_urls
     }
 
+def close_s3_client(s3_client: boto3.client):
+    """Close the S3 client."""
+    s3_client.close()
+
 def load_and_process_data() -> pd.DataFrame:
     access_key = config.AWS_ACCESS_KEY_ID
     secret_key = config.AWS_SECRET_ACCESS_KEY
@@ -58,26 +77,37 @@ def load_and_process_data() -> pd.DataFrame:
 
     s3_client = connect_to_s3(access_key, secret_key, endpoint=endpoint)
 
-    paginator = s3_client.get_paginator('list_objects_v2')
-    pages = paginator.paginate(Bucket=bucket, Prefix=prefix)
+    try:
+        paginator = s3_client.get_paginator('list_objects_v2')
+        pages = paginator.paginate(Bucket=bucket, Prefix=prefix)
 
-    result_dfs = []
-    # Process S3 objects and concatenate the results
-    for page in pages:
-        for obj in page['Contents']:
-            obj_data = s3_client.get_object(Bucket=bucket, Key=obj['Key'])['Body'].read()
-            result_dfs.append(process_object(obj_data, selected_fields))
-            del obj_data
+        result_dfs = []
+        # Process S3 objects and concatenate the results
+        for page in pages:
+            for obj in page['Contents']:
+                obj_data = s3_client.get_object(Bucket=bucket, Key=obj['Key'])['Body'].read()
+                result_dfs.append(process_object(obj_data, selected_fields))
 
-    # Concatenate dataframes
-    result_df = pd.concat(result_dfs, ignore_index=True)
+        # Concatenate dataframes
+        result_df = pd.concat(result_dfs, ignore_index=True)
 
-    publishers = set(result_df['publisher'].to_list())
+        publishers = set(result_df['publisher'].to_list())
 
-    data = []
-    for publisher in publishers:
-        publisher_urls = result_df['urls'][result_df['publisher'] == publisher].explode().to_list()
-        data.append({'publisher': publisher, 'urls': publisher_urls})
+        data = []
+        for publisher in publishers:
+            # Filter out NaN values fo each publisher
+            publisher_urls = result_df['urls'][result_df['publisher'] == publisher].explode().dropna().tolist()
+        
+            if publisher_urls:
+                data.append({'publisher': publisher, 'urls': publisher_urls})
 
-    urls_by_publisher = pd.DataFrame(data)
-    return urls_by_publisher
+        urls_by_publisher = pd.DataFrame(data)
+
+        with open(config.URLS_BY_PUBLISHER, 'w', newline='') as file:
+            file.truncate(0)
+            urls_by_publisher.to_csv(file, index=False)
+            logging.info(f"File saved: {file}")
+
+        return urls_by_publisher
+    finally:
+        close_s3_client(s3_client)
