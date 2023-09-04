2ed2a9e95ae04697e630eb12068b37d8511e453d
Wiktor Florian <wiktorflorianwf@gmail.com>
2023-09-04 09:49:20 +0200

    utils - doc string added

--- urls_response_codes/utils.py
+++ urls_response_codes/utils.py
@@ -1,7 +1,15 @@
 import re
 
-def extract_response_code(response_str):
-    match = re.search(r'\[(\d+)\s*(?:\w+\s*)*\]', response_str)
+def extract_response_code(response):
+    """
+    Extract the response code from a string containing a response.
+
+    Args:
+        response (str): The response string to extract the code from.
+    Returns:
+        int or None: The extracted response code as an integer, or None if not found
+    """
+    match = re.search(r'\[(\d+)\s*(?:\w+\s*)*\]', str(response))
     if match:
         return int(match.group(1))
     return None
